c8db575394174e85c48ee6d42834a58037304713
sintov <or.sintov@post.runi.ac.il>
2024-12-10 21:57:55 +0200

    seventh try

--- Scrabble.java
+++ Scrabble.java
@@ -108,7 +108,7 @@ public class Scrabble {
 				break;
 			}
 			if(!MyString.subsetOf(input, hand)){
-				System.out.println("Invalid word. Try again");
+				System.out.println("Invalid word. Try again.");
 			}else{
 				if(!isWordInDictionary(input)){
 					System.out.println("No such word in the dictionary. Try again");
